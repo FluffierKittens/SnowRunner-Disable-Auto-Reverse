94fce11bf30dd349803625b734763c298eb46de8
FluffierKittens <130815166+FluffierKittens@users.noreply.github.com>
2023-04-16 17:39:16 -0700

    Using MessageBoxW instead of MessageBox

--- Injector/injector.h
+++ Injector/injector.h
@@ -2,6 +2,6 @@
 #include <TlHelp32.h>
 
 #define SHOW_FATAL_ERROR(Msg) \
-	MessageBox(NULL, Msg, "DAR Injector: Fatal Error", MB_ICONERROR);
+	MessageBoxW(NULL, L##Msg, L"DAR Injector: Fatal Error", MB_ICONERROR);
 
 #define DAR_DLL_NAME L"DisableAutoReverse.dll"
